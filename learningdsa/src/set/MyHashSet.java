7a4bd26f6265a1d47008ea84290d6b879e8050a2
startcoding0to1 <startcoding0to1@gmail.com>
2024-03-25 10:02:17 +0530

    Reverse an Array...

--- learningdsa/src/set/MyHashSet.java
+++ learningdsa/src/set/MyHashSet.java
@@ -8,7 +8,7 @@ public class MyHashSet {
 		HashSet<Integer> set=new HashSet<>();
 		
 		//Insert - add
-		set.add(1);
+		System.out.println("Add  "+set.add(1));
 		set.add(2);
 		set.add(3);
 		set.add(1);//duplicates not allowed
